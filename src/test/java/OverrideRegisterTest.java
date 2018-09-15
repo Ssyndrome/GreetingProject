16026db386d534ad9190ec2ecd3bafdb516e5461
Ssyndrome <915448715@qq.com>
2018-09-15 10:05:16 +0800

    [Syndrom][2.AC2] be able to cover old chidren implementation class

--- src/test/java/OverrideRegisterTest.java
+++ src/test/java/OverrideRegisterTest.java
@@ -7,11 +7,21 @@ public class OverrideRegisterTest {
     void should_succeed_produce_instance_for_second_argument_in_register_bean() throws InstantiationException, IllegalAccessException {
         IoCContext context = new IoCContextIml();
         context.registerBean(MyBeanBase.class, MyBean.class);
-        Object myBeanBase = context.getBean(MyBeanBase.class);
+        Object myBeanBaseInstance = context.getBean(MyBeanBase.class);
 
-        MyBean expectedInstance = new MyBean();
-        assertEquals(expectedInstance.getClass(), myBeanBase.getClass());
+        Class expectedClass = MyBean.class;
+        assertEquals(expectedClass, myBeanBaseInstance.getClass());
     }
 
+    @Test
+    void should_cover_before_registered_class_after_second_register_by_parent() throws InstantiationException, IllegalAccessException {
+        IoCContext context = new IoCContextIml();
+        context.registerBean(MyBeanBase.class, MyBean.class);
 
+        context.registerBean(MyBeanBase.class, MyBeanCooler.class);
+        Object myBeanBaseInstance = context.getBean(MyBeanBase.class);
+
+        Class expectedClass = MyBeanCooler.class;
+        assertEquals(expectedClass, myBeanBaseInstance.getClass());
+    }
 }
